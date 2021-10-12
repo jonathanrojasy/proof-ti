cb19d19f909aac073372600b4ac0b6cb15dc62af
Jonathan Rojas <jonathanry98@gmail.com>
2021-10-11 19:24:48 -0500

    Init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let matriz;
+app.route('/resolver')
+ .post(function (req, res) {
+  if(!req.body.matriz) {
+   respuesta = {
+    error: true,
+    codigo: 502,
+    mensaje: 'Ingrese valores a la matriz'
+   };
+  } else {
+    matriz = req.body.matriz;
+    respuesta = {
+     error: false,
+     codigo: 200,
+     mensaje: 'Matriz invertida',
+     respuesta: matriz
+    };
+  }
+  res.send(respuesta);
+ })
+
+app.use(function(req, res, next) {
+    res.setHeader("Content-Type", "application/json");
+    next();
+    respuesta = {
+    error: true, 
+    codigo: 404, 
+    mensaje: 'URL no encontrada'
+    };
+    res.status(404).send(respuesta);
+});
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
